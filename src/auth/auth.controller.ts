46099f25603ecf6e64223e2282b5af44c9671817
Eliza Hynek <eliza.hynek@gmail.com>
2023-09-11 11:41:03 +0200

    add correct packeges

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import { Body, Controller, Post, UseGuards, Request } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { RegisterDTO } from './dtos/register.dto';
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { Injectable } from '@nestjs/common';
 import { UsersService } from 'src/users/users.service';
