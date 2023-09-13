a01635d0fe211a69f8aa3994798c2716a4bce6a2
Eliza Hynek <eliza.hynek@gmail.com>
2023-09-13 14:01:43 +0200

    configuration module

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,8 +1,9 @@
 /* eslint-disable prettier/prettier */
-import { Body, Controller, Post, UseGuards, Request, Response } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards, Request, Response, Delete } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { RegisterDTO } from './dtos/register.dto';
 import { LocalAuthGuard } from './local-auth.guard';
+import { JwtAuthGuard } from './jwt-auth.guard';
 
 @Controller('auth')
 export class AuthController {
@@ -18,6 +19,15 @@ export class AuthController {
   async login(@Request() req, @Response() res) {
     const tokens = await this.authService.createSession(req.user);
     res.cookie('auth', tokens, { httpOnly: true });
+    res.send({
+      message: 'success',
+    });
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Delete('logout')
+  async logout(@Response() res) {
+    res.clearCookie('auth', { httpOnly: true });
     res.send({
       message: 'success',
     });
