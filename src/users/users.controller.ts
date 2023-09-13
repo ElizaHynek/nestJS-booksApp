dbc0a4f1e20a940f565ff3e878d79a114570eee0
Eliza Hynek <eliza.hynek@gmail.com>
2023-09-13 14:34:45 +0200

    change users controller with users role

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,8 @@
 /* eslint-disable prettier/prettier */
-import { Controller, Get, NotFoundException, Param, ParseUUIDPipe } from '@nestjs/common';
+import { Controller, Delete, Get, NotFoundException, Param, ParseUUIDPipe, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { AdminAuthGuard } from 'src/auth/admin-auth.guard';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 
 @Controller('users')
 export class UsersController {
@@ -17,4 +19,14 @@ export class UsersController {
     if (!user) throw new NotFoundException('User not found');
     return user;
   }
+
+  @Delete(':id')
+	@UseGuards(AdminAuthGuard)
+	@UseGuards(JwtAuthGuard)
+	public async delete(@Param('id', new ParseUUIDPipe()) id: string) {
+	if (!(await this.usersService.getById(id)))
+		throw new NotFoundException('User not found');
+	await this.usersService.deleteById(id);
+	return { success: true };
+	}
 }
